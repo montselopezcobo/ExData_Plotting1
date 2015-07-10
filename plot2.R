6ac6b97d23ac555122e0d301a4111d478a6d750a
Montse López-Cobo <montse.lopezcobo@gmail.com>
2015-07-10 13:25:07 +0200

    first commit of course project files

---
+++ plot2.R
@@ -0,0 +1,36 @@
+######################################################################
+########                EXPLORATORY DATA ANALYSIS           ##########
+########                    COURSE PROJECT 1                ##########
+######################################################################
+
+                        ############
+                        #  PLOT 2  #
+                        ############
+
+###--- Download and unzip the data file
+
+fileUrl <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "./household_power_consumption.zip", method = "curl")
+dateDownloaded <- date()
+
+unzip("household_power_consumption.zip")
+
+###--- Read and subset the file
+DT <- fread("household_power_consumption.txt", sep=";", header=TRUE, 
+            na.strings = "?", colClasses = "character")
+DT2d <- DT[DT$Date %in% c("1/2/2007","2/2/2007"),]
+
+###--- Conversion of date and time to Data/Time classes
+DT2d$Time <- as.POSIXct(strptime(paste(DT2d$Date,DT2d$Time), 
+                                 "%d/%m/%Y %H:%M:%S"), origin="2007-02-01", tz="GMT")
+DT2d$Date <- as.Date(strptime(DT2d$Date, "%d/%m/%Y"))
+
+
+# PLOT 2
+#--------
+png(file="plot2.png")
+plot(DT2d$Time, as.numeric(DT2d$Global_active_power),
+     type="l",
+     xlab="",
+     ylab="Global active power (kilowatts)") 
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,46 @@
+######################################################################
+########                EXPLORATORY DATA ANALYSIS           ##########
+########                    COURSE PROJECT 1                ##########
+######################################################################
+
+                        ############
+                        #  PLOT 3  #
+                        ############
+
+###--- Download and unzip the data file
+
+fileUrl <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "./household_power_consumption.zip", method = "curl")
+dateDownloaded <- date()
+
+unzip("household_power_consumption.zip")
+
+###--- Read and subset the file
+DT <- fread("household_power_consumption.txt", sep=";", header=TRUE, 
+            na.strings = "?", colClasses = "character")
+DT2d <- DT[DT$Date %in% c("1/2/2007","2/2/2007"),]
+
+###--- Conversion of date and time to Data/Time classes
+DT2d$Time <- as.POSIXct(strptime(paste(DT2d$Date,DT2d$Time), 
+                                 "%d/%m/%Y %H:%M:%S"), origin="2007-02-01", tz="GMT")
+DT2d$Date <- as.Date(strptime(DT2d$Date, "%d/%m/%Y"))
+
+
+# PLOT 3
+#--------
+png(file="plot3.png")
+with (DT2d, {
+      plot(Time, as.numeric(Sub_metering_1),
+           col="black", 
+           type="l",
+           xlab="",
+           ylab="Energy sub metering")
+      lines(Time, as.numeric(Sub_metering_2),
+            col="red")
+      lines(Time, as.numeric(Sub_metering_3),
+            col="blue")
+      legend("topright", col = c("black", "red", "blue"), lty=1,
+             legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+})
+dev.off()
+
